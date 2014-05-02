8de65aa00b3741bd08a97cbb28b6a03fa0b52078
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-05-03 04:08:22 +0800

    Removed unwanted files.

--- .bin/npm-install-missing.cmd
+++
@@ -1,5 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\npm-install-missing\bin\npm-install-missing" %*
-) ELSE (
-  node  "%~dp0\..\npm-install-missing\bin\npm-install-missing" %*
-)
